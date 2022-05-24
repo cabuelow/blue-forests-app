174a8d92aa488236cd6f86a63679ed93b9c8f454
Chris Brown <christo.j.brown@gmail.com>
2022-05-24 21:28:35 +1000

    make simpler version of the app

--- server.R
+++ server.R
@@ -16,430 +16,74 @@ function(input, output, session) {
   ####################### Explore distributions logic ########################
   # create basemap
   
-  output$map <- renderLeaflet({
+  output$mymap <- renderLeaflet({
     leaflet() %>% 
       addProviderTiles(providers$CartoDB.Positron) %>% 
     addPolygons(
       group = 'basepoly',
       data = unitsall,
       color = "#FFFFFF",
-      weight = 0.4)
+      weight = 0.4) %>%
+    addCircleMarkers(group = "Blue Forest projects",
+      data = wwf,
+      color = ~pal(site_type),
+      weight = 1,
+      radius = 5,
+      popup= my_popups) %>%
+    addLegend("bottomright", data = wwf,
+              pal = pal, values = ~site_type,
+              title = "Project type",
+              opacity = 1) %>%
+      addLayersControl(
+        overlayGroups = c("Blue Forest projects"),
+        options = layersControlOptions(collapsed = FALSE)
+      )
   })
   
-  # reactive expression
+  #
+  # Filter data for ticked blue forests 
+  #
+  newdat <- reactive({
+    x <- filter(df, eco %in% as.numeric(input$blue_forest) &
+                       value ==1) 
+    x <- units2[units2$unit_ID %in% x$unit_ID, ]
+    return(x)
+  })
   
-  comb_output <- reactive({
-    
-    if(input$ct == 'Global'){ #if-else series that filters for polygons to map depending on if global & BF type
-      if(is.null(input$bf)){
-        filtdata <- unitsall
-        zz <- unname(st_bbox(filtdata))
-        cpal <- "#FFFFFF"
-      } else{
-       input_filter(df, input$bf)
-      }}else{
-        if(is.null(input$bf)){
-          filtdata <- unitsall
-          zz <- unname(st_bbox(filtdata))
-          cpal <- "#FFFFFF"
-        }else{
-          input_filter2(df, input$bf, input$ct)
-        }}
-    combo <- list(filtdata = filtdata, zz = zz, cpal = cpal)
-    
-  }) # end reactive
-  
-  # observe the reactive
-  
+  #
+  # Update map
+  #
+
   observe({
-
-    newdat <- comb_output() # get reactive data
-    
-    # create reactive map
-    
-      if(input$bfproj == FALSE){ # if-else to define whether wwf projects are on or off
-        leafletProxy("map") %>%
-          clearControls() %>% 
-          clearGroup(c('forest', 'profile')) %>% 
-          clearMarkers() %>% 
-          flyToBounds(newdat$zz[1], newdat$zz[2], newdat$zz[3], newdat$zz[4]) %>% 
-          addPolygons(
-            group = 'forest',
-            data = newdat$filtdata,
-            color = newdat$cpal,
-            weight = 0.4,
-            popup = T)
-      }else{
-        leafletProxy("map") %>%
-          clearControls() %>% 
-          clearGroup(c('forest', 'profile')) %>% 
-          flyToBounds(newdat$zz[1], newdat$zz[2], newdat$zz[3], newdat$zz[4]) %>% 
-          addPolygons(
-            group = 'forest',
-            data = newdat$filtdata,
-            color = newdat$cpal,
-            weight = 0.4,
-            popup = T) %>% 
-          addCircleMarkers(
-            data = wwf,
-            color = ~pal(site_type),
-            weight = 1,
-            radius = 5,
-            popup= my_popups) %>% 
-          addLegend("bottomright", data = wwf,
-                    pal = pal, values = ~site_type,
-                    title = "Project type",
-                    opacity = 1)
-      }
-  }) # end observe 
-
-  ####################### Explore hotspots logic ########################
   
-  # create basemap
-  
-  output$map1 <- renderLeaflet({
-    leaflet() %>% 
-      addProviderTiles(providers$CartoDB.Positron) %>% 
+    leafletProxy("mymap") %>%
+      # clearControls() %>%
+      clearGroup(c('forest')) %>%
       addPolygons(
-        group = 'basepoly1',
-        data = unitsall,
-        color = "#FFFFFF",
+        group = 'forest',
+        data = newdat(),
+        layerId=~unit_ID,
         weight = 0.4)
+    #note popups block shape_click events
+    
+    })
+  
+  #
+  # Render dashboard
+  #
+  
+  unit_ID_clicked <- reactive({
+    input$mymap_shape_click$id
   })
   
-  # observe input from slider and store value to use in reactive expression
-  
-  values <- reactiveValues(inDir = NULL)
-  observeEvent(input$perc, {values$inDir <- input$perc})
-  
-  # reactive expressions based on whether enabling constrain layer is on or off
-  
-  bindEvent( # enabling constraint off
-  comb_output1 <- reactive({
-    req(values$inDir);
-    if(input$profile2 == FALSE){
-    filt_hotspots(scores, input$bfcheck, 'mang', values$inDir)
-    }else{
-      filt_hotspots(scores2, input$bfcheck, 'mang', values$inDir)
+  output$unit_ID_dashboard <- renderTable({
+    if (!is.null(unit_ID_clicked())){
+    x <- filter(units2, unit_ID == unit_ID_clicked())
+    sf::st_geometry(x) <- NULL
+    x[,c("SOVEREIGN1", "unit_area_ha", "mangrove_2016_area_ha")]
+    } else {
+      NULL
     }
-  }), input$profile2, ignoreInit = TRUE)
-
-  # observe the reactive
+  })
   
-  observe({
-    
-    newdat1 <- comb_output1() # get reactive data
-    
-    if(input$profile2 == FALSE){ # if-else to define whether enabling constraints are on or off
-    if(input$bfproj2 == FALSE){ # if-else to define whether wwf projects are on or off
-    if(input$bfcheck != 'all'){  
-      
-      if(input$bfcheck == 'extent'){
-        sub <- 1
-      }else if(input$bfcheck == 'threat'){
-        sub <- 2
-      }else if(input$bfcheck == 'carbon'){
-        sub <- 3
-      }else if(input$bfcheck == 'cobenefit'){
-        sub <- 4
-      }else if(input$bfcheck == 'biodiversity'){
-        sub <- 5
-      }
-      
-      leafletProxy("map1") %>%
-        clearControls() %>% 
-        clearGroup(c('p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'profile')) %>%
-        clearMarkers() %>% 
-        addPolygons(
-          group = 'p1',
-          data = filter(units2, unit_ID %in% newdat1$unit_ID),
-          color = hot_pal[sub],
-          weight = 0.5) 
-    }else{
-      
-      a <- filter(newdat1, criteria == 'extent')
-      b <- filter(newdat1, criteria == 'threat')
-      c <- filter(newdat1, criteria == 'carbon')
-      d <- filter(newdat1, criteria == 'cobenefit')
-      e <- filter(newdat1, criteria == 'biodiversity')
-      
-      leafletProxy("map1") %>%
-        clearControls() %>% 
-        clearGroup(c('p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'profile')) %>%
-        clearMarkers() %>% 
-        addPolygons(
-          group = 'p1',
-          data = filter(units2, unit_ID %in% a$unit_ID),
-          color = hot_pal[1],
-          weight = 1) %>% 
-        addPolygons(
-          group = 'p2',
-          data = filter(units2, unit_ID %in% b$unit_ID),
-          color = hot_pal[2],
-          weight = 0.8) %>% 
-        addPolygons(
-          group = 'p3',
-          data = filter(units2, unit_ID %in% c$unit_ID),
-          color = hot_pal[3],
-          weight = 0.6) %>% 
-        addPolygons(
-          group = 'p4',
-          data = filter(units2, unit_ID %in% d$unit_ID),
-          color = hot_pal[4],
-          weight = 0.4) %>% 
-        addPolygons(
-          group = 'p5',
-          data = filter(units2, unit_ID %in% e$unit_ID),
-          color = hot_pal[5],
-          weight = 0.1) %>% 
-        addLegend("bottomright",
-                  colors = hot_pal, labels = c('Extent', 'Threat', 'Carbon', 'Cobenefit', 'Biodiversity'),
-                  #title = "Critera colour",
-                  opacity = 0.5)
-    }}else{
-      if(input$bfcheck != 'all'){  
-        
-        if(input$bfcheck == 'extent'){
-          sub <- 1
-        }else if(input$bfcheck == 'threat'){
-          sub <- 2
-        }else if(input$bfcheck == 'carbon'){
-          sub <- 3
-        }else if(input$bfcheck == 'cobenefit'){
-          sub <- 4
-        }else if(input$bfcheck == 'biodiversity'){
-          sub <- 5
-        }
-        
-        leafletProxy("map1") %>%
-          clearControls() %>% 
-          clearGroup(c('p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'profile')) %>%
-          clearMarkers() %>% 
-          addPolygons(
-            group = 'p1',
-            data = filter(units2, unit_ID %in% newdat1$unit_ID),
-            color = hot_pal[sub],
-            weight = 0.5) %>% 
-          addCircleMarkers(
-            data = wwf,
-            color = ~pal(site_type),
-            weight = 1,
-            radius = 5,
-            popup= my_popups)
-      }else{
-        
-        a <- filter(newdat1, criteria == 'extent')
-        b <- filter(newdat1, criteria == 'threat')
-        c <- filter(newdat1, criteria == 'carbon')
-        d <- filter(newdat1, criteria == 'cobenefit')
-        e <- filter(newdat1, criteria == 'biodiversity')
-        
-        leafletProxy("map1") %>%
-          clearControls() %>% 
-          clearGroup(c('p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'profile')) %>%
-          clearMarkers() %>% 
-          addPolygons(
-            group = 'p1',
-            data = filter(units2, unit_ID %in% a$unit_ID),
-            color = hot_pal[1],
-            weight = 1) %>% 
-          addPolygons(
-            group = 'p2',
-            data = filter(units2, unit_ID %in% b$unit_ID),
-            color = hot_pal[2],
-            weight = 0.8) %>% 
-          addPolygons(
-            group = 'p3',
-            data = filter(units2, unit_ID %in% c$unit_ID),
-            color = hot_pal[3],
-            weight = 0.6) %>% 
-          addPolygons(
-            group = 'p4',
-            data = filter(units2, unit_ID %in% d$unit_ID),
-            color = hot_pal[4],
-            weight = 0.4) %>% 
-          addPolygons(
-            group = 'p5',
-            data = filter(units2, unit_ID %in% e$unit_ID),
-            color = hot_pal[5],
-            weight = 0.1) %>% 
-          addCircleMarkers(
-            data = wwf,
-            color = ~pal(site_type),
-            weight = 1,
-            radius = 5,
-            popup= my_popups) %>% 
-          addLegend("bottomright",
-                    pal = hot_pal, values = c('Extent', 'Threat', 'Carbon', 'Cobenefit', 'Biodiversity'),
-                    #title = "Critera colour",
-                    opacity = 1)
-      }
-    }}else{ # end wwf proj ifelse. start profile ifelse
-      if(input$bfproj2 == FALSE){ # if-else to define whether wwf projects are on or off
-      if(input$bfcheck != 'all'){  
-        
-        if(input$bfcheck == 'extent'){
-          sub <- 1
-        }else if(input$bfcheck == 'threat'){
-          sub <- 2
-        }else if(input$bfcheck == 'carbon'){
-          sub <- 3
-        }else if(input$bfcheck == 'cobenefit'){
-          sub <- 4
-        }else if(input$bfcheck == 'biodiversity'){
-          sub <- 5
-        }
-        
-        leafletProxy("map1") %>%
-          clearControls() %>% 
-          clearGroup(c('p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'profile')) %>%
-          clearMarkers() %>% 
-          addPolygons(
-            group = 'profile',
-            data = profile1.sf,
-            color = "#FFCC99",
-            weight = 0.4) %>% 
-          addPolygons(
-            group = 'p1',
-            data = filter(units2, unit_ID %in% newdat1$unit_ID),
-            color = hot_pal[sub],
-            weight = 0.5) 
-      }else{
-        
-        a <- filter(newdat1, criteria == 'extent')
-        b <- filter(newdat1, criteria == 'threat')
-        c <- filter(newdat1, criteria == 'carbon')
-        d <- filter(newdat1, criteria == 'cobenefit')
-        e <- filter(newdat1, criteria == 'biodiversity')
-        
-        leafletProxy("map1") %>%
-          clearControls() %>% 
-          clearGroup(c('p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'profile')) %>%
-          clearMarkers() %>% 
-          addPolygons(
-            group = 'profile',
-            data = profile1.sf,
-            color = "#FFCC99",
-            weight = 0.4) %>% 
-          addPolygons(
-            group = 'p1',
-            data = filter(units2, unit_ID %in% a$unit_ID),
-            color = hot_pal[1],
-            weight = 1) %>% 
-          addPolygons(
-            group = 'p2',
-            data = filter(units2, unit_ID %in% b$unit_ID),
-            color = hot_pal[2],
-            weight = 0.8) %>% 
-          addPolygons(
-            group = 'p3',
-            data = filter(units2, unit_ID %in% c$unit_ID),
-            color = hot_pal[3],
-            weight = 0.6) %>% 
-          addPolygons(
-            group = 'p4',
-            data = filter(units2, unit_ID %in% d$unit_ID),
-            color = hot_pal[4],
-            weight = 0.4) %>% 
-          addPolygons(
-            group = 'p5',
-            data = filter(units2, unit_ID %in% e$unit_ID),
-            color = hot_pal[5],
-            weight = 0.1) %>% 
-          addLegend("bottomright",
-                    colors = hot_pal, labels = c('Extent', 'Threat', 'Carbon', 'Cobenefit', 'Biodiversity'),
-                    #title = "Critera colour",
-                    opacity = 0.5)
-      }}else{
-        if(input$bfcheck != 'all'){  
-          
-          if(input$bfcheck == 'extent'){
-            sub <- 1
-          }else if(input$bfcheck == 'threat'){
-            sub <- 2
-          }else if(input$bfcheck == 'carbon'){
-            sub <- 3
-          }else if(input$bfcheck == 'cobenefit'){
-            sub <- 4
-          }else if(input$bfcheck == 'biodiversity'){
-            sub <- 5
-          }
-          
-          leafletProxy("map1") %>%
-            clearControls() %>% 
-            clearGroup(c('p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'profile')) %>% 
-            clearMarkers() %>% 
-            addPolygons(
-              group = 'profile',
-              data = profile1.sf,
-              color = "#FFCC99",
-              weight = 0.4) %>% 
-            addPolygons(
-              group = 'p1',
-              data = filter(units2, unit_ID %in% newdat1$unit_ID),
-              color = hot_pal[sub],
-              weight = 0.5) %>% 
-            addCircleMarkers(
-              data = wwf,
-              color = ~pal(site_type),
-              weight = 1,
-              radius = 5,
-              popup= my_popups)
-        }else{
-          
-          a <- filter(newdat1, criteria == 'extent')
-          b <- filter(newdat1, criteria == 'threat')
-          c <- filter(newdat1, criteria == 'carbon')
-          d <- filter(newdat1, criteria == 'cobenefit')
-          e <- filter(newdat1, criteria == 'biodiversity')
-          
-          leafletProxy("map1") %>%
-            clearControls() %>% 
-            clearGroup(c('p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'profile')) %>%
-            clearMarkers() %>% 
-            addPolygons(
-              group = 'profile',
-              data = profile1.sf,
-              color = "#FFCC99",
-              weight = 0.4) %>% 
-            addPolygons(
-              group = 'p1',
-              data = filter(units2, unit_ID %in% a$unit_ID),
-              color = hot_pal[1],
-              weight = 1) %>% 
-            addPolygons(
-              group = 'p2',
-              data = filter(units2, unit_ID %in% b$unit_ID),
-              color = hot_pal[2],
-              weight = 0.8) %>% 
-            addPolygons(
-              group = 'p3',
-              data = filter(units2, unit_ID %in% c$unit_ID),
-              color = hot_pal[3],
-              weight = 0.6) %>% 
-            addPolygons(
-              group = 'p4',
-              data = filter(units2, unit_ID %in% d$unit_ID),
-              color = hot_pal[4],
-              weight = 0.4) %>% 
-            addPolygons(
-              group = 'p5',
-              data = filter(units2, unit_ID %in% e$unit_ID),
-              color = hot_pal[5],
-              weight = 0.1) %>% 
-            addCircleMarkers(
-              data = wwf,
-              color = ~pal(site_type),
-              weight = 1,
-              radius = 5,
-              popup= my_popups) %>% 
-            addLegend("bottomright",
-                      pal = hot_pal, values = c('Extent', 'Threat', 'Carbon', 'Cobenefit', 'Biodiversity'),
-                      #title = "Critera colour",
-                      opacity = 1)
-        }
-      }
-    }
-  })# end observe
- 
 } #end server
